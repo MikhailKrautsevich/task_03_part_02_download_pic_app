24f2f8e62011d5782d0c82ba5d90184b2859d355
MikhailKrautsevich <mihail4egevara@gmail.com>
2022-04-09 01:11:19 +0300

    сделал layout заимплементил Пикассо

--- app/src/main/java/com/example/downloadpicapp/MainActivity.kt
+++ app/src/main/java/com/example/downloadpicapp/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.downloadpicapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.core.widget.addTextChangedListener
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mEditText: EditText
+    private lateinit var mImageView: ImageView
+    private lateinit var mButton: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mEditText = findViewById(R.id.main_edittext)
+        mImageView = findViewById(R.id.main_imageview)
+        mButton = findViewById(R.id.btn_get_random)
+
+        mEditText.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+        })
     }
 }
